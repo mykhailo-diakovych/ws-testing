46274cf92987ee0c5989fa6c6c0277f64b7bd76b
mykhailo <m.dikovych13@gmail.com>
2023-04-11 16:14:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <button @click="connect">connect</button>
+  </div>
+</template>
+<script setup lang="ts">
+const connect = () => {
+    new WebSocket('ws://dev.amlglobal.net/chat/ws/39Fo28ftHyDoZQxwMfq8Gd/?token=3de5fab876c9603f37db55449c52ab0b6d30f1ef')
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
